c980273eb2c93ffcdd35a248656a89a12e4446ab
Mendy Laufer <laufermendy@gmail.com>
2023-06-25 11:27:27 +0300

    fileHub

---
+++ fileDepartment.py
@@ -0,0 +1,16 @@
+class Department:
+    def __init__(self, department_id, department_name, manager_id, hub_id):
+        self.department_id = department_id
+        self.department_name = department_name
+        self.manager_id = manager_id
+        self.hub_id = hub_id
+
+    def setManager_id(self, manager_id):
+        self.manager_id = manager_id
+
+    def setHub_id(self, hub_id):
+        self.hub_id = hub_id
+
+    def dEmployees_sum(self):
+        # Your implementation here
+        pass
