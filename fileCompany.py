0d816c0edc79498497fbd7400d49aaad1d7a903c
EvyatarRevach <avi0548163@gmail.com>
2023-06-27 15:19:46 +0300

    3rd_code_that_works

--- fileCompany.py
+++ fileCompany.py
@@ -11,11 +11,11 @@ class Company:
         self.db = SqlQuery()
 
     def add_employee(self, employee):
-        query = f"INSERT INTO employee (employee_id, role_id, manager_id, first_name, last_name, email_address, phone_number, hire_date, salary) VALUES ({employee.employee_id}, {employee.role_id}, {employee.manager_id}, '{employee.first_name}', '{employee.last_name}', '{employee.email_address}', '{employee.phone_number}', '{employee.hire_date}', {employee.salary})"
+        query = f"INSERT INTO employee (employee_id, role_id, first_name, last_name, email_address, phone_number, hire_date, salary ,department_id, hub_id) VALUES ({employee.employee_id}, {employee.role_id}, '{employee.first_name}', '{employee.last_name}', '{employee.email_address}', '{employee.phone_number}', '{employee.hire_date}', {employee.salary}, {employee.department_id}, {employee.hub_id})"
         self.db.query_set(query)
 
     def update_employee(self, employee):
-        query = f"UPDATE employee SET role_id = {employee.role_id}, manager_id = {employee.manager_id}, first_name = '{employee.first_name}', middle_name = '{employee.middle_name}', last_name = '{employee.last_name}', email_address = '{employee.email_address}', phone_number = '{employee.phone_number}', hire_date = '{employee.hire_date}', salary = {employee.salary} WHERE employee_id = {employee.employee_id}"
+        query = f"UPDATE employee SET role_id = {employee.role_id},  first_name = '{employee.first_name}', last_name = '{employee.last_name}', email_address = '{employee.email_address}', phone_number = '{employee.phone_number}', hire_date = '{employee.hire_date}', salary = {employee.salary} WHERE employee_id = {employee.employee_id}"
         self.db.query_set(query)
 
     def delete_employee(self, employee_id):
@@ -33,6 +33,6 @@ class Company:
 
 
 
-company = Company("CEO Name", "CTO Name", "CFO Name")
-employee = Employee(209, 1, 1, "John", "Doe", "john@example.com", "123456789", "2023-06-27", 5000.0)
-company.add_employee(employee)
+# company = Company("CEO Name", "CTO Name", "CFO Name")
+# employee = Employee(209,  1, "John", "Doe", "john@example.com", "123456789", "2023-06-27", 5000.0)
+# employee.set_hub_id()
--- main.py
+++ main.py
@@ -1,4 +1,38 @@
-x = ("Git-Test1 ")
-y = ("Mendy Laufer ")
+from fileCompany import Company
+from fileEmployee import Employee
+from employee_manager import EmployeeManager
 
-print(f"hi {x}  {y}" )
+
+
+company = Company("CEO Name", "CTO Name", "CFO Name")
+number = int(input("For Employees Management Access Press: 1 \nFor Budgets Management Access Press: 2 \nFor Hubs and Departments Management Access Press: 3\n> "))
+if number == 1:
+    choice = int(input("to add employee press 1:\n to delete employee press 2:\n to change employee's details press 3:\n> "))
+    if choice == 1:
+        # Get employee details from user input
+        employee_id = int(input("Enter employee ID: "))
+        role_id = int(input("Enter role ID: "))
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        email_address = input("Enter email address: ")
+        phone_number = input("Enter phone number: ")
+        hire_date = input("Enter hire date (YYYY-MM-DD): ")
+        salary = float(input("Enter salary: "))
+        department_id = int(input("Enter department ID: "))
+        hub_id = int(input("Enter hub ID: "))
+
+        # Create Employee object
+        employee = Employee(employee_id, role_id, first_name, last_name, email_address, phone_number, hire_date, salary,
+                            department_id, hub_id)
+
+        # Add employee to the company
+        company.add_employee(employee)
+
+        print("Employee added successfully.")
+
+elif number == 2:
+    pass
+# employee = Employee(209,  1, "John", "Doe", "john@example.com", "123456789", "2023-06-27", 5000.0)
+# employee.set_hub_id()
+
+
